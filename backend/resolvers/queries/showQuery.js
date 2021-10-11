3101b0d76b4da46ebf894bf49c2c5532ee4d6722
John Knecht V <jjk1492@rit.edu>
2021-10-10 20:01:02 -0400

    Refactored showQuery too use the new context object for auth and username checking

--- backend/resolvers/queries/showQuery.js
+++ backend/resolvers/queries/showQuery.js
@@ -3,18 +3,18 @@ import User from '../../models/user'
 import { UserError } from 'graphql-errors'
 import { ADMIN } from '../../constants'
 
-const isRequestingOwnUser = (req, args) => {
-  return req.auth.username !== undefined &&
-        req.auth.username === args.studentUsername
+const isRequestingOwnUser = (context, args) => {
+  return context.username !== undefined &&
+        context.username === args.studentUsername
 }
 
-export function show (_, args, req) {
+export function show (_, args, context) {
   return Show.findByPk(args.id)
 }
 
-export function shows (_, args, req) {
+export function shows (_, args, context) {
   // Students can only look at their own shows
-  if (args.studentUsername && req.auth.type !== ADMIN && !isRequestingOwnUser(req, args)) {
+  if (args.studentUsername && context.authType !== ADMIN && !isRequestingOwnUser(context, args)) {
     throw new UserError('Permission Denied')
   }
 
@@ -34,7 +34,7 @@ export function shows (_, args, req) {
           .then((entries) => {
             const showIds = entries.map(entry => entry.showId)
             // Predicate that matches shows open for submissions
-            const isOpen = req.auth.type !== ADMIN
+            const isOpen = context.authType !== ADMIN
               ? {
                   $and: [
                     { entryStart: { $lt: new Date() } },
