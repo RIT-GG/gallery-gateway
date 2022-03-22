e2605ad46a5eee8d2a556fe13ea5fabc40dd243a
ajw5817 <42786190+ajw5817@users.noreply.github.com>
2022-03-22 18:17:03 -0400

    portfolio period details tab complete

--- frontend/src/Admin/components/portfolio/PortfolioPeriodDetailsTab.js
+++ frontend/src/Admin/components/portfolio/PortfolioPeriodDetailsTab.js
@@ -1,17 +1,44 @@
-import React from "react";
-import { Col, Container, Row } from "reactstrap";
+import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Button } from 'reactstrap'
+import moment from 'moment'
+
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import FaEdit from '@fortawesome/fontawesome-free-solid/faEdit'
+
 
 function PortfolioPeriodDetailsTab(props){
 
-    return (
-        <Container fluid>
-            <Row>
-                <Col xs="12">
-                    <h1>Portfolio Period Details</h1>
-                </Col>
-            </Row>
-        </Container>
-    )
+  return (
+    <Fragment>
+      <Row>
+        <Col>
+          {props.portfolioPeriod.description ? (
+            <Fragment>
+              <dt>Description</dt>
+              <dd>{props.portfolioPeriod.description}</dd>
+            </Fragment>
+          ) : null}
+          <dt>Individual Submission Limit</dt>
+          <dd>{props.portfolioPeriod.entryCap}</dd>
+        </Col>
+        <Col>
+          <Button
+            color='dark'
+            className='text-left'
+            outline
+            block
+            tag={Link}
+            to={`/portfolio-period/${props.portfolioPeriod.id}/edit`}
+            disabled={!moment(props.portfolioPeriod.judgingEndDate).isAfter(moment())}
+          >
+            <FontAwesomeIcon icon={FaEdit} className='align-middle' /> 
+            Edit Portfolio Period Details
+          </Button>
+        </Col>
+      </Row>
+    </Fragment>
+  )
 }
 
 export default PortfolioPeriodDetailsTab;
--- frontend/src/Admin/containers/portfolio/PortfolioPeriodDetailsTab.js
+++ frontend/src/Admin/containers/portfolio/PortfolioPeriodDetailsTab.js
@@ -1,10 +1,10 @@
-import { graphql } from 'react-apollo'
 import { connect } from 'react-redux'
 import { compose } from 'recompose'
 
 import PortfolioPeriodDetailsTab from '../../components/portfolio/PortfolioPeriodDetailsTab'
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
+
   })
 
 export default compose(
