55593e67b07d6e87468f3e0541bdcd50c81896b2
John Knecht V <jjk1492@rit.edu>
2022-03-23 12:38:04 -0400

    Comments and cleanup

--- frontend/src/Student/components/scholarships/Scholarships.js
+++ frontend/src/Student/components/scholarships/Scholarships.js
@@ -5,16 +5,22 @@ import ScholarshipCard from "./ScholarshipCard";
 function Scholarships(props) {
     const { scholarships } = props
 
+    // Handle rending inavlid or no scholarships
+    if (!Array.isArray(scholarships) || scholarships.length === 0) {
+        return (
+            <Container>
+                <h1 className="mt-3 mb-5">Scholarships</h1>
+                <p>There are no scholarships to apply to</p>
+            </Container>
+        )
+    }
+
     return (
         <Container>
             <h1 className="mt-3 mb-5">Scholarships</h1>
-            {!Array.isArray(scholarships) || scholarships.length === 0 ? (
-                <p>There are no scholarships to apply to</p>
-            ) : (
-                <React.Fragment>
-                    {scholarships.map(scholarship => <ScholarshipCard scholarship={scholarship} />)}
-                </React.Fragment>
-            )}
+            <React.Fragment>
+                {scholarships.map(scholarship => <ScholarshipCard scholarship={scholarship} />)}
+            </React.Fragment>
         </Container>
     )
 }
