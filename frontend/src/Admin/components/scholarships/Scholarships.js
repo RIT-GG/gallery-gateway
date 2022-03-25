01b4d7baa65a0a501e63c42f1dbeb9914d07c305
John Knecht V <jjk1492@rit.edu>
2022-03-25 00:48:52 -0400

    Create the Admin Scholarships containter and component

---
+++ frontend/src/Admin/components/scholarships/Scholarships.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react"
+import { Col, Container, Row } from "reactstrap"
+import ScholarshipCard from "../../../Student/components/scholarships/ScholarshipCard";
+
+function Scholarships(props) {
+    const { scholarships } = props
+    const [inactiveScholarships, setInactiveScholarships] = useState([])
+    const [activeScholarships, setActiveScholarships] = useState([])
+
+    useEffect(() => {
+        let inactive = []
+        let active = []
+
+        if (Array.isArray(scholarships) && scholarships.length > 0) {
+            for (let idx = 0; idx < scholarships.length; idx++) {
+                let scholarship = scholarships[idx]
+                console.log(scholarship)
+                scholarship.active ? active.push(scholarship) : inactive.push(scholarship)
+            }
+        }
+
+        setInactiveScholarships(inactive)
+        setActiveScholarships(active)
+
+    }, [scholarships])
+
+
+    console.log(props)
+    // Handle invalid or no scholarships
+    if (!Array.isArray(scholarships) || scholarships.length === 0) {
+        return (
+            <Container fluid>
+                <Row>
+                    <Col xs={12} className="px-0">
+                        <p>There are no scholarships.</p>
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
+
+    return (
+        <Container fluid>
+            <Row>
+                <Col xs={12} className="px-0">
+                    <h3 className="text-success">Active Scholarships</h3>
+                    {
+                        activeScholarships.map(scholarship => {
+                            return <ScholarshipCard scholarship={scholarship} />
+                        })
+                    }
+                </Col>
+                <Col xs={12} className="px-0">
+                    <h3 className="text-warning">Inactive Scholarships</h3>
+                    {
+                        inactiveScholarships.map(scholarship => {
+                            return <ScholarshipCard scholarship={scholarship} />
+                        })
+                    }
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Scholarships
